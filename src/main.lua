1850e865a06caa111e2eec248aac43c8db21b287
Undeadsewer <undeadsewer@gmail.com>
2017-11-11 17:48:42 -0500

    Edited main

--- src/main.lua
+++ src/main.lua
@@ -7,7 +7,7 @@ local handSprite = sprite {
     y = love.graphics.getHeight() / 2 - 50,
     w = 300,
     h = 100,
-    visible = false
+    visible = true
 }
 
 local handSpriteOutline = sprite {
@@ -19,10 +19,12 @@ local handSpriteOutline = sprite {
     shader = love.graphics.newShader("outline.glsl")
 }
 
-handSpriteOutline.shader:send("stepSize", {1/handSpriteOutline.image:getWidth(), 1/handSpriteOutline.image:getHeight()})
-handSpriteOutline.shader:send("color", {0.5, 1.0, 0.5})
+handSpriteOutline.shader:send("stepSize", {5/handSpriteOutline.image:getWidth(), 5/handSpriteOutline.image:getHeight()})
+handSpriteOutline.shader:send("color", {241/255, 66/255, 244/255})
 
 function love.update(dt)
+	handSpriteOutline.shader:send("stepSize", {math.random(5,20)/handSpriteOutline.image:getWidth(), math.random(5,20)/handSpriteOutline.image:getHeight()})
+	handSpriteOutline.shader:send("color", {math.random(0,255)/255, math.random(0,255)/255, math.random(0,255)/255})
 end
 
 function love.draw()
